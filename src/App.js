bd3ed97a0737d02fb231c35c89dc36e16346efd5
mounika.neerati <mounika.neerati93@gmail.com>
2024-02-11 21:51:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React, { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from "react"
+import './App.css'
+function App() {
 
-const App = () => {
-  const [counter,setCounter] = useState(1);
+    const [Time, setTime] = useState(new Date().toLocaleTimeString());
 
-  let num1=2;
-  let num2= 30;
+    const [products, setProducts] = useState(
+        [
+            { id: 1, name: 'One Plus Nord CE2', price: 20000 },
+            { id: 2, name: 'Oppo F21 S', price: 22000 },
+            { id: 3, name: 'Samsung Galaxy  M 33', price: 17000 },
+        ]
+    )
 
-  let sum = useMemo(()=>{
-    return num1+num2
 
-  },[num1,num2])
+    const [searchText, updateSearchText] = useState('');
 
-  return (
-    <>
-      <button onClick={()=>{
-        setCounter(counter+1);
-      }}> Update Counter </button>
+    const filteredProducts = useMemo(
 
-      <h1>Counter :{counter}</h1>
-      <p>Sum :{sum} </p>
-    </>
-  )
+        () => {
+
+            console.log(products)
+
+            return products.filter(
+                (p) => {
+
+                    return p.name.toLowerCase().includes(searchText.toLowerCase())
+
+                }
+            )
+
+        },
+        [products, searchText]
+
+    )
+
+
+    useEffect(
+        () => {
+            setInterval(
+                () => {
+                    setTime(new Date().toLocaleTimeString())
+
+                }, 1000
+            )
+
+        }, []
+    )
+
+    return (
+        <>
+
+            <div>{Time}</div>
+
+            <input type="text" value={searchText} onChange={e => updateSearchText(e.target.value)} />
+
+            <table>
+                <tr>
+                    <td> S.No</td>
+                    <td>Name</td>
+                    <td>Price</td>
+                </tr>
+                {
+                    filteredProducts.map(
+                        p => (
+                            <tr key={p.id}>
+                                <td> {p.id}</td>
+                                <td>{p.name}</td>
+                                <td>{p.price}</td>
+                            </tr>
+
+                        )
+                    )
+                }
+            </table>
+        </>
+    )
+
 }
 
 export default App
