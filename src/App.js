dc8413c22faca341d0a534fd1cc550aff90eed45
mounika.neerati <mounika.neerati93@gmail.com>
2024-02-13 21:50:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import React, { lazy,Suspense,useState } from 'react'
+import React from "react";
 
+function AuthCheck(Component){
+  let authenticated = true; 
 
-const Form =lazy(()=>import('./components/form'))
+  return (props)=>{
+    if(authenticated){
+      return <Component {...props}/>
+    }
+    else{
+      return <Login/>
+    }
+  }
 
-const App = () => {
-  
-  const [show,setShow]=useState(false)
+}
 
+function User(props){
   return (
     <>
-    <div className='text-center'>
-      <h1>Lazy Loading</h1>
-      <button className='btn btn-primary'
-      onClick={()=>{
-        setShow(true)
-      }}>Open</button>
-    </div>
-    {
-      show===true && (
-        <Suspense>
-          <Form/>
-        </Suspense>
-      )
-    }
-    </>
+    <h1 className="text-info">This Is <span className="text-primary">User</span></h1>
+  <h2>Hello <span className="text-success">{props.username}</span></h2></>
+  )
+}
+
+function Login(){
+  return <h1>This Login Component</h1>
+}
+
+const AuthChecked = AuthCheck(User)
+
+function App(){
+  return (
+    <AuthChecked username={'Mounika Neerati'}/>
+
   )
 }
 
